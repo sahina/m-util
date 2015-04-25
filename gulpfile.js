dd9aa8ad205a9c2a6bdbf9f2c5a11c511d193d07
Altug Sahin <altug@aecoffice.com>
2015-04-25 17:38:49 -0400

    added jspm build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,71 @@
+var path = require('path');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var del = require('del');
 var runSequence = require('run-sequence');
 
-
-gulp.task('clean', function(cb) {
+gulp.task('clean', function (cb) {
   return del('dist/**', cb);
 });
 
-gulp.task('test-html', function(cb) {
+gulp.task('test-assets', function () {
   return gulp.src(['test/**'])
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('font', function() {
+gulp.task('font', function () {
   return gulp.src('lib/font/**/*')
     .pipe(gulp.dest('dist/font'));
 });
 
-gulp.task('scss', function() {
+gulp.task('scss', function () {
   return gulp.src('lib/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', function(cb) {
+gulp.task('test', function (cb) {
   runSequence(
-    ['clean'], ['font', 'scss', 'test-html', 'watch-test'],
+    ['clean'], ['font', 'scss', 'test-assets', 'bundle', 'watch-test'],
     cb
   );
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch('lib/scss/**/*.scss', ['scss']);
+  gulp.watch('lib/js/**/*.js', ['bundle']);
 });
 
-gulp.task('watch-test', function() {
+gulp.task('watch-test', function () {
   gulp.watch('lib/scss/**/*.scss', ['scss']);
-  gulp.watch(['test/**'], ['test-html']);
+  gulp.watch(['test/**', 'index.html'], ['test-assets']);
 });
 
-gulp.task('build', function(cb) {
+gulp.task('bundle', function (cb) {
+  var Builder = require('systemjs-builder');
+  var builder = new Builder({
+    sourceMaps: true,
+    transpiler: 'babel'
+  });
+
+  builder.loadConfig('./jspm.config.js')
+    .then(function () {
+
+      builder.config({baseURL: 'file:' + process.cwd()});
+      builder.build('lib/js/bootstrap', 'dist/bundle.js')
+        .then(function () {
+          return cb();
+        })
+        .catch(function (ex) {
+          console.log(ex);
+          cb(new Error(ex));
+        });
+    });
+});
+
+gulp.task('build', function (cb) {
   runSequence(
-    ['clean'], ['clean', 'font', 'scss', 'watch'],
+    ['clean'], ['font', 'scss', 'bundle', 'watch'],
     cb
   );
 });
--- jspm.config.js
+++ jspm.config.js
@@ -18,6 +18,7 @@ System.config({
     "babel": "npm:babel-core@5.1.13",
     "babel-runtime": "npm:babel-runtime@5.1.13",
     "core-js": "npm:core-js@0.8.4",
+    "fians/Waves": "github:fians/Waves@0.7.0",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
